88fdafa0efbaa2f0a0d8563913bfdd6ee9cb178a
RustemGimazetdinov <grfbox@mail.ru>
2015-03-26 13:37:52 +0300

    fix

---
+++ tr111/tr111.HTMLClient/Screens/AddEditCustomerItem.lsml.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditCustomerItem.created = function (screen) {
+    // Write code here.
+    screen.details.displayName = "Добавление нового клиента";
+    screen.CustomerItem.FName = "Петр";
+    screen.findContentItem("Email").isVisible = false;
+};
